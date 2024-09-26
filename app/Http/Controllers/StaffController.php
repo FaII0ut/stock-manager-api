cce20e0732301b0eb9bb56eddf7c03bcd10211d6
Naif Mohamed <naif.mohamed@pension.gov.mv>
2024-09-26 07:03:20 +0500

    Implement search and sorting functionality in StaffController index method fix #4

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -10,9 +10,48 @@ use Illuminate\Http\Request;
 
 class StaffController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return StaffResource::collection(Staff::query()->latest('id')->paginate());
+        $query = Staff::query();
+
+        // Single search parameter
+        if ($request->has('search')) {
+            $search = $request->input('search');
+            $query->where(function ($q) use ($search) {
+                $q->where('name', 'like', '%' . $search . '%')
+                  ->orWhere('nid', 'like', '%' . $search . '%')
+                  ->orWhere('staff_code', 'like', '%' . $search . '%')
+                  ->orWhere('phone', 'like', '%' . $search . '%');
+            });
+        }
+
+        // Individual field searches
+        if ($request->has('name')) {
+            $query->where('name', 'like', '%' . $request->input('name') . '%');
+        }
+
+        if ($request->has('nid')) {
+            $query->where('nid', 'like', '%' . $request->input('nid') . '%');
+        }
+
+        if ($request->has('staff_code')) {
+            $query->where('staff_code', 'like', '%' . $request->input('staff_code') . '%');
+        }
+
+        if ($request->has('phone')) {
+            $query->where('phone', 'like', '%' . $request->input('phone') . '%');
+        }
+
+        // Sort results
+        $sortBy = $request->input('sort_by', 'id');
+        $sortOrder = $request->input('sort_order', 'desc');
+        $query->orderBy($sortBy, $sortOrder);
+
+        // Paginate results
+        $perPage = $request->input('per_page', 15);
+        $staff = $query->latest('id')->paginate($perPage);
+
+        return StaffResource::collection($staff);
     }
 
     public function store(StoreStaffRequest $request)
